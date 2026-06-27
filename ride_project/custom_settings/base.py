ce7af86dfc37ae4c1232cb95da2feeab48b873c9
Jaime Andres Perez Hinestroza <andresprzh@gmail.com>
2026-06-27 11:42:00 -0500

    create an commons app, where custom coode used by others abpp will be located and add a custom pagination here to use by the REST API views

--- ride_project/custom_settings/base.py
+++ ride_project/custom_settings/base.py
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'apps.commons.pagination.BasePagination',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ]
