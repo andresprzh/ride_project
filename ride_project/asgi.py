f698234372d1390d8a989221dfdafebdbb750c81
Jaime Andres Perez Hinestroza <andresprzh@gmail.com>
2026-06-27 03:00:55 -0500

    Raname project

--- ride_project/asgi.py
+++ ride_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_template project.
+ASGI config for ride_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- ride_project/wsgi.py
+++ ride_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_template project.
+WSGI config for ride_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
